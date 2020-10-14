f80f72b35377d3d80ceb5f261475488ac9a30b6c
snabelA00 <ja000411@gmail.com>
2020-10-14 15:19:05 +0200

    Gave value to indexes in ticTacToeBoard array

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -2,5 +2,28 @@ package com.company;
 
 public class TicTacToe {
 
+    //create board
+    char[][] ticTacToeBoard = new char[3][3];
 
+    // give board cells index value 1-9
+    public TicTacToe() {
+        for (int i = 0; i < ticTacToeBoard.length; i++){
+
+            for(int j = 0; j < ticTacToeBoard.length; j++){
+                //give value to array elements
+                ticTacToeBoard[i][j] = ("|"+ ((i+1) * (j+1)) + "|" ).charAt(1);
+                //System.out.println((i+1) * (j+1));
+            }
+        }
+
+        for(int i = 0; i < ticTacToeBoard.length; i++){
+            for(int j = 0; j < ticTacToeBoard.length; j++) {
+                System.out.println("|     " + ticTacToeBoard[i][j] + "     |");
+            }
+            System.out.println("-----------------------------------------------");
+        }
+        System.out.println(ticTacToeBoard[0][0]);
+    }
+
+    //create a visual board in terminal
 }
